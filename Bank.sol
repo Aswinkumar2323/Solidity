6bcd35050252c270462ca8336bb7a3d3176836f0
Aswin Kumar <89674911+Aswinkumar2323@users.noreply.github.com>
2024-01-21 20:03:04 +0530

    Create Bank.sol

---
+++ Bank.sol
@@ -0,0 +1,17 @@
+pragma solidity >=0.5.0 < 0.9.0;
+
+contract Bank{
+    address payable public owner;
+
+    constructor() payable{
+        owner = payable(msg.sender);
+    }
+
+    function withdraw(uint256 amount) external {
+        require(msg.sender == owner, "caller is not owner");
+        payable(owner).transfer(amount);
+    }
+    function getbal() external view returns(uint256){
+        return address(this).balance;
+    }
+}
